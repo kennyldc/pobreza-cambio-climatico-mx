898e862ad2f4586ba3a9205e950422bce606d148
kennyldc <caribaz@gmail.com>
2023-04-23 17:00:51 -0600

    agrego la creacion de categorías en el merge

--- modulo-enoe/merge-ENOE-desastres/merge-enoe-emergencias.R
+++ modulo-enoe/merge-ENOE-desastres/merge-enoe-emergencias.R
@@ -455,8 +455,13 @@ urbanas_enoe_con_fenomenos <- urbanas_enoe_con_fenomenos |>
          "heladas_granizadas_nevadas_ultimos_1800" = 0,
          "inundacion_ciclon_tropical_ultimos_1800" = 0)
 
-# Agregamos una columna que servirá después como identificador de las personas. 
-# La columna junta el lugar donde vive y su fecha de nacimiento
+# Para poder ver cuántas personas diferentes existen en la base necesitamos un identificador por persona
+# En las encuestas de la ENOE este dato no existe por lo que es necesario
+# construirle un identificador "sintético" o imputado a cada persona.
+# La alternativa más viable para solucionar este problema consiste en usar la fecha de nacimiento y 
+# los identificadores de su hogar. Esta es prácticamente la única solución 
+# posible para identificar a las personas de manera única.
+
 urbanas_enoe_con_fenomenos <- urbanas_enoe_con_fenomenos |>
   mutate(identificador_persona = paste0(upm,"-",n_pro_viv,"-",v_sel, "-", n_hog, "-", nac_dia,"-", nac_mes, "-", nac_anio))
 
@@ -483,5 +488,125 @@ enoe_fenomenos <- rbind(urbanas_enoe_con_fenomenos,
 
 # Al igual que antes, podemos guardar ese dataset (nuestro output)
 # Este será el que utilicemos para el análisis y los modelos de regresión
+enoe_fenomenos |> 
+  saveRDS(file = "enoe_fenomenos_nueva.rds")
+
+# Ajustes a la base post-algoritmo para comenzar las estimaciones ---------
+
+# Con este datset tenemos la información de todos los participantes de la ENOE (tantas veces como hayan respondido la encuesta)
+# junto con columnas que indican la cantidad de desastres a la que han sido expuestos
+# en los últimos 0 a 90 días, 91 a 180 días, 181 a 360 días, 361 a 540 días, 
+# 541 a 720 días y 721 a 1800 días
+# La información viene desagregada por tipo de fenómeno según la información original de la base de declaratorias:
+# - actividad volcanica
+# - ciclon tropical lluvias
+# - deslave
+# - deslizamiento
+# - fuertes vientos
+# - granizadas
+# - heladas granizadas
+# - heladas
+# - hundimientos
+# - incendio forestal
+# - inundacion ciclon tropical
+# - inundacion
+# - lluvias ciclon tropical
+# - lluvias inundacion
+# - lluvias
+# - nevadas, heladas, granizadas,
+# - nevadas
+# - sismos
+# - temperatura extrema
+# - tornado
+
+# Adicionalmente se incluye la suma de desastres a la que ha estado expuesto en la misma cantidad de días:
+# - suma_ultimos_180
+# - suma_ultimos_1800
+# - suma_ultimos_540
+# - suma_ultimos_720
+# - suma_ultimos_90
+
+# Problemas con el identificador:
+# En pasos previos de este código creamos un identificador por persona el cuál 
+# sale de combinar la fecha de nacimiento con alguna información identificadora del hogar
+# Sin embargo, uno de los ajustes post-algoritmo retoma este asunto
+# ya que esa solución (además de ser la única) tiene algunos problemas que necesitan
+# ajustarse antes de utilizar la base para las estimaciones.
+
+# Notamos que algunos de los identificadores no 
+# son útiles puesto que las personas no supieron responder su fecha de nacimiento.
+# Lamentablemente no hay manera de reparar este problema dado que sin la 
+# fecha de nacimiento no hay manera de identificar de manera única a las personas
+# El problema afecta al 8% de la base que dada su magnitud parece ser un problema menor
+
+numero_de_veces_que_aparece_persona <- enoe_fenomenos |>
+  filter(nac_dia != "99" & nac_mes != "99" & nac_anio != "9999") |>
+  group_by(identificador_persona) |>
+  count()
+
+# Otro problema que se identificó fue con respecto a las personas que comparten fecha de nacimiento y viven en la misma casa
+# Lógicamente esto les hace compartir identificadores: año_nac, mes_nac, dia_nac, upm, n_pro_viv, v_sel y n_hog
+# Con ellos, tampoco hay manera de identificarlos únicamente.
+# Si bien se podría utilizar el número de renglón (n_ren) para formar la llave, tampoco hay
+# certeza de que de una entrevista a otra las personas hubiesen estado en el mismo renglón.
+# El problema de las personas que tienen la misma fecha afecta al 1% de la base
+# No hay ninguna otra manera de obtener un indicador único sobre las personas fuera de los ya utilizados
+
+# Dado esto, debemos filtrar nuestra base para que solo aparezcan las personas que están en 
+# de 1 a 5 veces, recordando que 5 es el número máximo de veces que una persona puede contestar la enoe
+# Creamos un vector con los identificadores de las personas que aparecen de 1 a 5 veces
+personas_5_o_menos <- numero_de_veces_que_aparece_persona |>
+  filter(n<=5) |>
+  pull(identificador_persona)
+
+# Filtramos la base enoe_fenomenos para solo tener a las personas que aparecen de 1 a 5 veces
+enoe_fenomenos <- enoe_fenomenos |>
+  filter(identificador_persona %in% personas_5_o_menos)
+
+# Notamos que mientras el tamaño original de enoe_fenomenos (post algoritmo) era de 7,283,301
+# Con el ajuste obtenemos 6,587,307 observaciones. Bastante útil para continuar con el analisis
+
+# El último ajuste relevante consistió en crear categorías de tipo de desastre más precisas 
+# para agrupar todas las que venían originalmente en CENAPRED.
+# Por ejemplo, en CENAPRED original era posible identificar tipos de desastre: 
+# i) inundación, ii) lluvias y iii) lluvias e inundación. 
+# Esto es un problema en el sentido que nos está dando información redundante y que bien podríamos agrupar todo en una misma categoría: lluvias.
+
+# Algo similar pasa con otros de los tipos de desastres por lo que optamos por las siguientes categorías:
+#   
+# Lluvias e inundaciones
+# Ciclón tropical
+# Temperaturas extremas
+# Heladas y nevadas
+
+enoe_fenomenos <- enoe_fenomenos |> 
+  mutate(categoria_lluvias_e_inundaciones_ultimos_90 = inundacion_ultimos_90 + lluvias_ultimos_90,  # no hay "lluvias_inundacion 90"
+         categoria_lluvias_e_inundaciones_ultimos_180 = inundacion_ultimos_180 + lluvias_ultimos_180 + lluvias_inundacion_ultimos_180,
+         categoria_lluvias_e_inundaciones_ultimos_360 = inundacion_ultimos_360 + lluvias_ultimos_360 + lluvias_inundacion_ultimos_360,
+         categoria_lluvias_e_inundaciones_ultimos_540 = inundacion_ultimos_540 + lluvias_ultimos_540 + lluvias_inundacion_ultimos_540,
+         categoria_lluvias_e_inundaciones_ultimos_720 = inundacion_ultimos_720 + lluvias_ultimos_720 + lluvias_inundacion_ultimos_720,
+         categoria_lluvias_e_inundaciones_ultimos_1800 = inundacion_ultimos_1800 + lluvias_ultimos_1800 + lluvias_inundacion_ultimos_1800,
+         categoria_ciclon_tropical_ultimos_90 = ciclon_tropical_lluvias_ultimos_90 + ciclon_tropical_ultimos_90 + lluvias_ciclon_tropical_ultimos_90, # no hay "inundacion_ciclon_tropical 90"
+         categoria_ciclon_tropical_ultimos_180 = ciclon_tropical_lluvias_ultimos_180 + ciclon_tropical_ultimos_180 + lluvias_ciclon_tropical_ultimos_180 + inundacion_ciclon_tropical_ultimos_180,
+         categoria_ciclon_tropical_ultimos_360 = ciclon_tropical_lluvias_ultimos_360 + ciclon_tropical_ultimos_360 + lluvias_ciclon_tropical_ultimos_360, # no hay "inundacion_ciclon_tropical 360"
+         categoria_ciclon_tropical_ultimos_540 = ciclon_tropical_lluvias_ultimos_540 + ciclon_tropical_ultimos_540 + lluvias_ciclon_tropical_ultimos_540 + inundacion_ciclon_tropical_ultimos_540,
+         categoria_ciclon_tropical_ultimos_720 = ciclon_tropical_lluvias_ultimos_720 + ciclon_tropical_ultimos_720 + lluvias_ciclon_tropical_ultimos_720, # no hay "inundacion_ciclon_tropical_720
+         categoria_ciclon_tropical_ultimos_1800 = ciclon_tropical_lluvias_ultimos_1800 + ciclon_tropical_ultimos_1800 + lluvias_ciclon_tropical_ultimos_1800 + inundacion_ciclon_tropical_ultimos_1800,
+         categoria_heladas_y_nevadas_ultimos_90 = heladas_ultimos_90 + nevadas_heladas_granizadas_ultimos_90 + nevadas_ultimos_90,
+         categoria_heladas_y_nevadas_ultimos_180 = heladas_ultimos_180 + nevadas_heladas_granizadas_ultimos_180 + nevadas_ultimos_180,
+         categoria_heladas_y_nevadas_ultimos_360 = heladas_ultimos_360 + nevadas_heladas_granizadas_ultimos_360 + nevadas_ultimos_360,
+         categoria_heladas_y_nevadas_ultimos_540 = heladas_ultimos_540 + nevadas_heladas_granizadas_ultimos_540 + nevadas_ultimos_540,
+         categoria_heladas_y_nevadas_ultimos_720 = heladas_ultimos_720 + nevadas_heladas_granizadas_ultimos_720 + nevadas_ultimos_720,
+         categoria_heladas_y_nevadas_ultimos_1800 = heladas_ultimos_1800 + nevadas_heladas_granizadas_ultimos_1800 + nevadas_ultimos_1800 + heladas_granizadas_nevadas_ultimos_1800) |> 
+  rename(categoria_temperaturas_extremas_ultimos_90 = temperatura_extrema_ultimos_90,
+         categoria_temperaturas_extremas_ultimos_180 = temperatura_extrema_ultimos_180,
+         categoria_temperaturas_extremas_ultimos_360 = temperatura_extrema_ultimos_360,
+         categoria_temperaturas_extremas_ultimos_540 = temperatura_extrema_ultimos_540,
+         categoria_temperaturas_extremas_ultimos_720 = temperatura_extrema_ultimos_720,
+         categoria_temperaturas_extremas_ultimos_1800 = temperatura_extrema_ultimos_1800)
+
+# Esta base es la que podemos guardar de cara a las estadísticas
+# descriptivas y a las estimaciones
+
 enoe_fenomenos |> 
   saveRDS(file = "enoe_fenomenos_nueva.rds")
